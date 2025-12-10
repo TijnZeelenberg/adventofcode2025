4c61665547f366d300098714fbd352408be9ff54
Tijn Zeelenberg <tijnqz@gmail.com>
2025-12-10 19:23:41 +0100

    Add solution for part 1 of day 5

--- day5.py
+++ day5.py
@@ -1,7 +1,28 @@
-file = "testinput.txt"
+file = "day5_input.txt"
 
 with open(file) as f:
-  lines = f.read()
+    lines = f.read()
+rangeIDSplit = lines.split("\n\n")
 
-freshRanges = [range.split("-") for range in lines.split("\n\n")[0].split("\n")]
-print(freshRanges)
+freshRanges = [
+    [int(rangestr.split("-")[0]), int(rangestr.split("-")[1])]
+    for rangestr in rangeIDSplit[0].split("\n")
+]
+produceIDs = [int(ID) for ID in rangeIDSplit[1].split("\n")[:-1]]
+
+print("freshRanges = ", freshRanges)
+print("\n\n\n ProduceIDs = ", produceIDs)
+
+fresh = False
+freshIDs = []
+for ID in produceIDs:
+    for rangelist in freshRanges:
+        if (ID >= rangelist[0]) and (ID <= rangelist[1]):
+            fresh = True
+            break
+    if fresh == True:
+        freshIDs.append(ID)
+    fresh = False
+
+print(freshIDs)
+print("Part one answer = ", len(freshIDs))
